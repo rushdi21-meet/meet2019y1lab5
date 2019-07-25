c1ad84d91992d8560da4ecab3a25a3b6664d74c5
rushdi21-meet <rushdi21@meet.mit.edu>
2019-07-25 05:41:10 -0400

    Stopping here

---
+++ lab_5.py
@@ -0,0 +1,6 @@
+a =['she','sells','sea','shells','by','the','sea','shore']
+b = "selfish shellfish"
+c = [1, 1, 2, 3, 5, 8, 13]
+one = [1,2,3,4]
+two = [7,6,5,4]
+three = ["y1", "friends", "fun"]
---
+++ test_list.py
@@ -0,0 +1,6 @@
+def test_list(our_list):
+  test_list=input print("e")
+  if len(test_list)>2 and test_list[0] == test_list[-1]:
+    return("True")
+  else :
+    return("False")
